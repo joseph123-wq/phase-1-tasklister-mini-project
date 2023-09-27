d4392c07c3fdc4fd530354a625a12fd204962ef1
JosephKagwi <josephkagwi28@gmail.com>
2023-09-28 00:22:01 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTaskForm = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+
+  
+    const newTaskDescription = document.getElementById("new-task-description").value;
+
+    
+    addTaskToList(newTaskDescription);
+
+  
+    document.getElementById("new-task-description").value = "";
+  });
+
+  function addTaskToList(description) {
+    
+    const listItem = document.createElement("li");
+
+    
+    listItem.innerHTML = `${description} <button class="remove-task">X</button>`;
+
+    
+    taskList.appendChild(listItem);
+
+    
+    const removeButton = listItem.querySelector(".remove-task");
+    removeButton.addEventListener("click", () => {
+      listItem.remove();
+    });
+  }
 });
+
+
